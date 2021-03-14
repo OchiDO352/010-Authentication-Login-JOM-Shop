1ffdcd6c032102ac8a7b2b1e88b6ac6001f1726f
OchiDO352 <75597102+OchiDO352@users.noreply.github.com>
2021-03-14 13:31:01 +0700

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,11 @@
+export default {
+    firebaseConfig : {
+        apiKey: "AIzaSyCZ996gdT5MLw4twuFAmwaWb0-c3Ogm4b4",
+        authDomain: "my-cloud-firestore-98b5f.firebaseapp.com",
+        projectId: "my-cloud-firestore-98b5f",
+        storageBucket: "my-cloud-firestore-98b5f.appspot.com",
+        messagingSenderId: "428268522902",
+        appId: "1:428268522902:web:274d70b9e721dc6852a1b7",
+        measurementId: "G-NN4H5FHVM8"
+      }
+};
